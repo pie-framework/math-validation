ffbac14ee885a272c98b20656decea0f379aa1d6
ed eustace <ed.eustace@gmail.com>
2020-06-04 14:14:57 +0100

    fix up tests - add filter

--- test/equal.test.ts
+++ test/equal.test.ts
@@ -3,34 +3,52 @@ import { basename, resolve } from "path";
 import * as CSON from "cson";
 import { readFileSync } from "fs-extra";
 import { equal } from "../src/index";
+import * as minimist from "minimist";
 
-const fixtures = sync(`fixtures/${basename(__filename, ".test.ts")}/*.cson`, {
+const doubleDashIndex = process.argv.indexOf("--");
+const splitArgv = process.argv.slice(doubleDashIndex);
+const args = minimist(
+  process.argv.slice(doubleDashIndex ? doubleDashIndex + 1 : 2)
+);
+
+const getPattern = () => {
+  const b = basename(__filename, ".test.ts");
+  const filter = args.filter || "";
+  const out = `fixtures/${b}/*${filter}*.cson`;
+  // console.log("getPattern:", out, b, filter);
+  return out;
+};
+
+const fixtures = sync(getPattern(), {
   cwd: resolve(__dirname, ".."),
 });
 
 fixtures.forEach((f) => {
-  const raw = readFileSync(f, "utf-8");
-  const data = CSON.parse(raw);
-  // console.log(JSON.stringify(data, null, "  "));
-  describe(f, () => {
-    data.tests.forEach((t) => {
-      describe(t.label || t.target, () => {
-        const eq = t.eq ? (Array.isArray(t.eq) ? t.eq : [t.eq]) : [];
+  try {
+    const raw = readFileSync(f, "utf-8");
+    const data = CSON.parse(raw);
+    describe(f, () => {
+      data.tests.forEach((t) => {
+        describe(t.label || t.target, () => {
+          const eq = t.eq ? (Array.isArray(t.eq) ? t.eq : [t.eq]) : [];
 
-        eq.forEach((y) => {
-          it(` == ${y}`, () => {
-            expect(equal(t.target, y, { foo: true })).toEqual(true);
+          eq.forEach((y) => {
+            it(` == ${y}`, () => {
+              expect(equal(t.target, y, { foo: true })).toEqual(true);
+            });
           });
-        });
 
-        const ne = t.ne ? (Array.isArray(t.ne) ? t.ne : [t.ne]) : [];
+          const ne = t.ne ? (Array.isArray(t.ne) ? t.ne : [t.ne]) : [];
 
-        ne.forEach((y) => {
-          it(`!= ${y}`, () => {
-            expect(equal(t.target, y, { foo: true })).not.toEqual(true);
+          ne.forEach((y) => {
+            it(`!= ${y}`, () => {
+              expect(equal(t.target, y, { foo: true })).not.toEqual(true);
+            });
           });
         });
       });
     });
-  });
+  } catch (e) {
+    console.error(e);
+  }
 });
