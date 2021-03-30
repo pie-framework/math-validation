6580536c8445a07ee375fb18e66446052aa1c41f
ed eustace <ed.eustace@gmail.com>
2021-03-30 15:53:25 +0100

    new test report

--- src/fixtures/latex-equal/literal.ts
+++ src/fixtures/latex-equal/literal.ts
@@ -2,25 +2,31 @@ export default {
   mode: "literal",
   tests: [
     {
+      target: "12 + 3 * x",
+      eq: ["12    +     3   \\times x  "],
+      ne: ["12.0 + 3 * x", "3 * x + 12"],
+      opts: { allowTrailingZeros: false },
+    },
+    {
       target: "12.00",
       eq: ["12.000000"],
       ne: ["12.001"],
-      literalOpts: { allowTrailingZeros: true },
+      opts: { allowTrailingZeros: true },
     },
     {
       target: "12.00",
       eq: ["12.00"],
       ne: ["12.000000"],
-      literalOpts: { allowTrailingZeros: false },
+      opts: { allowTrailingZeros: false },
     },
-    { target: "2 + 3", eq: ["3 + 2"], literalOpts: { ignoreOrder: true } },
-    { target: "2 + 3", ne: ["3 + 2"], literalOpts: { ignoreOrder: false } },
-    { target: "a+b+c", eq: ["c+a+b"], literalOpts: { ignoreOrder: true } },
-    { target: "a+b+c", ne: ["c+a+b"], literalOpts: { ignoreOrder: false } },
+    { target: "2 + 3", eq: ["3 + 2"], opts: { ignoreOrder: true } },
+    { target: "2 + 3", ne: ["3 + 2"], opts: { ignoreOrder: false } },
+    { target: "a+b+c", eq: ["c+a+b"], opts: { ignoreOrder: true } },
+    { target: "a+b+c", ne: ["c+a+b"], opts: { ignoreOrder: false } },
     {
       target: "12.00+3",
       eq: ["3+12.00"],
-      literalOpts: { allowTrailingZeros: true, ignoreOrder: true },
+      opts: { allowTrailingZeros: true, ignoreOrder: true },
     },
   ],
 };
