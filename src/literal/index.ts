835779cf4babf1a38406f384e09b56971b499a11
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2021-04-02 16:50:46 +0300

    parse tzn from ast to mathjs and add allowTrailingZeros to literal parsing

--- src/literal/index.ts
+++ src/literal/index.ts
@@ -1,5 +1,8 @@
 import { MathNode } from "mathjs";
 import { logger } from "../log";
+const { create, all } = require("mathjs");
+
+const mathjs = create(all);
 
 export type LiteralOpts = {
   allowTrailingZeros?: boolean;
@@ -13,7 +16,25 @@ export type LiteralOpts = {
 
 const log = logger("mv:literal");
 
+const simplifyRule = { l: "tzn(n1, n2)", r: "n1" };
+
 export const isMathEqual = (a: MathNode, b: MathNode, opts: LiteralOpts) => {
+  if(typeof a === "string") {
+    a = mathjs.parse(a);
+  }
+
+  if(typeof b === "string") {
+    b = mathjs.parse(b);
+  }
+
+  if (opts && opts.allowTrailingZeros) {
+    a = mathjs.simplify(a, [simplifyRule]);
+    b = mathjs.simplify(b, [simplifyRule]);
+  }
+
   log("a:", a.toTex(), "b: ", b.toTex());
-  return a.equals(b);
+
+  const equalTex = a.toTex().trim() === b.toTex().trim();
+
+  return a.equals(b) || equalTex;
 };
