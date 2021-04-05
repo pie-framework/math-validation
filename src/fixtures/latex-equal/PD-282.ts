8595cc659b14534eb367ada8aa51c7204ff3a3eb
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2021-04-05 12:40:14 +0300

    update PD-282 fixture

--- src/fixtures/latex-equal/PD-282.ts
+++ src/fixtures/latex-equal/PD-282.ts
@@ -6,16 +6,16 @@ export default {
   tests: [
     {
       opts: {
-        literal: { allowTrailingZeros: true}
+        literal: { allowTrailingZeros: true }
       },
       target: "1",
-      eq: ["1.0", "01"], // 01 is a leading zero, not a trailing zero
+      eq: ["1.0", "01"],
       ne: ["1.01"],
       triage: Triage.FLOATING_POINT,
     },
     {
       opts: {
-        allowTrailingZeros: false,
+        literal: { allowTrailingZeros: false }
       },
       target: "1",
       eq: ["01"],
