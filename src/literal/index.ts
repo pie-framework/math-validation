14d1108fa75bb70e2eb838cb883a55e05c42ad83
Patricia Romaniuc <romaniucpatricia@yahoo.com>
2021-04-06 12:55:28 +0300

    remove string parsing, isMathEqual should only accept MathNodes

--- src/literal/index.ts
+++ src/literal/index.ts
@@ -19,14 +19,6 @@ const log = logger("mv:literal");
 const simplifyRule = { l: "tzn(n1, n2)", r: "n1" };
 
 export const isMathEqual = (a: MathNode, b: MathNode, opts: LiteralOpts) => {
-  if(typeof a === "string") {
-    a = mathjs.parse(a);
-  }
-
-  if(typeof b === "string") {
-    b = mathjs.parse(b);
-  }
-
   if (opts && opts.allowTrailingZeros) {
     a = mathjs.simplify(a, [simplifyRule]);
     b = mathjs.simplify(b, [simplifyRule]);
