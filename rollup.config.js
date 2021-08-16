e612f1f6bbbd62f69dc9a43300da9bed3a56bd04
Carla <carlacostea@yahoo.com>
2021-08-16 13:43:06 +0300

    remove console.logs and comments

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-
+import strip from "@rollup/plugin-strip";
 import sucrase from "@rollup/plugin-sucrase";
+import cleanup from 'rollup-plugin-cleanup';
 
 const rollupConfig = {
   input: "src/demo.ts",
@@ -14,7 +15,13 @@ const rollupConfig = {
     resolve({ browser: true, mainFields: ["module"] }),
     commonjs({}),
     sucrase({ transforms: ["typescript"] }),
+    cleanup({ extensions: ["js", "ts"], compactComments: true }),
+    strip({
+      functions: ["console.*", 'debug',],
+      extensions: ["js", "ts"]
+    }),
   ],
+
 };
 
 export default [rollupConfig];
