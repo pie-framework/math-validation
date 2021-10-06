449d2cde835798b92687236f1f137da9270cd8db
Carla Costea <carlacostea@yahoo.com>
2021-10-06 15:28:19 +0300

    add test, I found a problem in current aproach

--- src/fixtures/latex-equal/symbolic/2-way-inequalities-with-variables.ts
+++ src/fixtures/latex-equal/symbolic/2-way-inequalities-with-variables.ts
@@ -28,6 +28,15 @@ export default {
          ],
        },
        {
+        target: "2x<4",
+        eq: [
+          "4>2x",
+          "x+x < 2*2",      
+        ],
+        // this is a problem, when multiplying both parts with a negative number, direction should be changed, expressions should not be equivalent anymore  
+        ne: ["x*(-1)<2*(-1)"]
+      },
+       {
          target: "2x+y≤4+y",
          eq: [
            "4≥2x",
@@ -43,7 +52,18 @@ export default {
           "x+x +3y≤ 2*2 +y",
           "4x+6y≤8+2y",
         ],
-        ne: ["x<2", "3x≤4", "x<3"]
+        ne: ["x+y<2", "3x+2y≤4", "x+4y≤3"]
+      },
+      {
+        target: "5.5x + 8y≥100",
+        eq: [
+          "11x+16y≥200",
+          "5.5x + 8y+10≥100+10",
+          "5.5x + 8y+z≥100+z",
+          "(5.5x + 8y)*2≥100*2",
+          "5.5x + 8y -100≥0",
+          "(5.5x + 8y)≥100",
+        ],
       },
   ],
 };
