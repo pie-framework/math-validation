d1036a194361cb205607dd38568aef3af0b9c5d9
Carla Costea <carlacostea@yahoo.com>
2021-10-11 15:04:24 +0300

    break compound inequalities

--- src/symbolic/compare-compound-inequations.ts
+++ src/symbolic/compare-compound-inequations.ts
@@ -1,44 +1,72 @@
 import { mathjs } from "../mathjs";
-import {  MathNode } from "mathjs";
+import { MathNode } from "mathjs";
 
 const m: any = mathjs;
 
-const operation = (signName:string) => {
-    if (signName === 'larger'){
-      return ">"
-    } 
-  
-    return "≥"
+export type InequalitiesPairs = {
+  leftHandInequality: MathNode;
+  rightHandInequality: MathNode;
+};
+
+const operation = (signName: string) => {
+  if (signName === "larger") {
+    return ">";
   }
 
-const breakInequalities = (compoundInequality: any) => {
-    let firstInequality: MathNode;
-    let secondInequality: MathNode;
-  
-    if(compoundInequality.conditionals?.length === 2 && compoundInequality.params?.length === 3 ){
-      firstInequality = new m.OperatorNode(operation(compoundInequality.conditionals[0]),compoundInequality.conditionals[0],[compoundInequality.params[0],compoundInequality.params[1]] )
-    }
-  
-    console.log(firstInequality, "first ine")
+  console.log(signName, "signName");
+  return "≥";
+};
+
+const breakInequality = (compoundInequality: any) => {
+  let inequalities: InequalitiesPairs;
+
+   inequalities= {
+    leftHandInequality: new m.OperatorNode(
+      operation(compoundInequality.conditionals[0]),
+      compoundInequality.conditionals[0],
+      [compoundInequality.params[0], compoundInequality.params[1]]
+    ),
+    rightHandInequality: new m.OperatorNode(
+      operation(compoundInequality.conditionals[1]),
+      compoundInequality.conditionals[0],
+      [compoundInequality.params[1], compoundInequality.params[2]]
+    ),
   };
 
-export const compareCompoundInequations = (firstInequation:any, secondInequation:any ) => {
-let equality: boolean = false;
+  console.log(compoundInequality.conditionals, "conditionals");
 
-//@ts-ignore
-const result = firstInequation.conditionals.every((relation: string) =>
-//@ts-ignore
-secondInequation.conditionals.includes(relation)
-);
+  console.log(
+    inequalities.leftHandInequality,
+    inequalities.rightHandInequality,
+    "first inequality"
+  );
 
-if (!result) {
-equality = false;
-} else {
-const firstInequalities = breakInequalities(firstInequation);
-const secondInequalities = breakInequalities(secondInequation);
-}
+  return inequalities;
+};
 
-console.log(result, "result");
+export const compareCompoundInequations = (
+  firstInequation: any,
+  secondInequation: any
+) => {
+  let equality: boolean = false;
 
-return equality;
-}
+  //@ts-ignore
+  const result = firstInequation.conditionals.every((relation: string) =>
+    //@ts-ignore
+    secondInequation.conditionals.includes(relation)
+  );
+
+  if (!result) {
+    equality = false;
+  } else if (
+    firstInequation.conditionals?.length === 2 &&
+    firstInequation.params?.length === 3
+  ) {
+    const firstInequalities = breakInequality(firstInequation);
+    const secondInequalities = breakInequality(secondInequation);
+  }
+
+  console.log(result, "result");
+
+  return equality;
+};
