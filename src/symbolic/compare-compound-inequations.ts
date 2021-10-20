2be847aa83aff8fea8069b4c7a02480d49754d15
Carla Costea <carlacostea@yahoo.com>
2021-10-20 15:11:02 +0300

    refactor

--- src/symbolic/compare-compound-inequations.ts
+++ src/symbolic/compare-compound-inequations.ts
@@ -1,6 +1,13 @@
 import { mathjs } from "../mathjs";
-import  {  MathNode } from "mathjs";
-import { expressionsCanBeCompared, equationsHaveTheSameUnknowns, getUnknowns, transformEqualityInExpression, getCoefficients, solveLinearEquation } from "./utils";
+import { MathNode } from "mathjs";
+import {
+  expressionsCanBeCompared,
+  equationsHaveTheSameUnknowns,
+  getUnknowns,
+  transformEqualityInExpression,
+  getCoefficients,
+  solveLinearEquation,
+} from "./utils";
 
 const m: any = mathjs;
 
@@ -16,34 +23,39 @@ export type xRange = {
 
 const operation = (signName: string) => (signName === "larger" ? ">" : "≥");
 
-const breakInequality = (compoundInequality: any): InequalitiesPairs => {
-  return {
-    leftHandInequality: new m.OperatorNode(
-      operation(compoundInequality.conditionals[0]),
-      compoundInequality.conditionals[0],
-      [compoundInequality.params[0], compoundInequality.params[1]]
-    ),
-    rightHandInequality: new m.OperatorNode(
-      operation(compoundInequality.conditionals[1]),
-      compoundInequality.conditionals[0],
-      [compoundInequality.params[1], compoundInequality.params[2]]
-    ),
-  };
-};
+const breakInequality = (compoundInequality: any): InequalitiesPairs => ({
+  leftHandInequality: new m.OperatorNode(
+    operation(compoundInequality.conditionals[0]),
+    compoundInequality.conditionals[0],
+    [compoundInequality.params[0], compoundInequality.params[1]]
+  ),
+  rightHandInequality: new m.OperatorNode(
+    operation(compoundInequality.conditionals[1]),
+    compoundInequality.conditionals[0],
+    [compoundInequality.params[1], compoundInequality.params[2]]
+  ),
+});
 
-export const getLimit = (expressionsPair :InequalitiesPairs, limitType:string):number => {
-  const expressionR = transformEqualityInExpression(expressionsPair.rightHandInequality);
-  const expressionL = transformEqualityInExpression(expressionsPair.leftHandInequality);
+export const getLimit = (
+  expressionsPair: InequalitiesPairs,
+  limitType: string
+): number => {
+  const expressionR = transformEqualityInExpression(
+    expressionsPair.rightHandInequality
+  );
+  const expressionL = transformEqualityInExpression(
+    expressionsPair.leftHandInequality
+  );
 
   const xFirstInequality = solveLinearEquation(getCoefficients(expressionR));
-  const xSecondInequality =solveLinearEquation(getCoefficients(expressionL));
+  const xSecondInequality = solveLinearEquation(getCoefficients(expressionL));
 
   if (limitType === "inferior") {
-    return Math.min(xFirstInequality,xSecondInequality)
+    return Math.min(xFirstInequality, xSecondInequality);
   } else {
-    return Math.max(xFirstInequality,xSecondInequality)
+    return Math.max(xFirstInequality, xSecondInequality);
   }
-}
+};
 
 export const compareCompoundInequations = (
   firstInequation: any,
@@ -55,36 +67,43 @@ export const compareCompoundInequations = (
     (relation: string) =>
       secondInequation.conditionals.includes(relation) &&
       firstInequation.conditionals?.length === 2 &&
-      firstInequation.params?.length === 3 && expressionsCanBeCompared(firstInequation,secondInequation)
+      firstInequation.params?.length === 3 &&
+      expressionsCanBeCompared(firstInequation, secondInequation)
   );
 
   if (!result) {
     return false;
-  } 
-    const firstInequalityUnknownsName = getUnknowns(firstInequation);
-    const secondInequalityUnknownsName = getUnknowns(secondInequation);
+  }
+  const firstInequalityUnknownsName = getUnknowns(firstInequation);
+  const secondInequalityUnknownsName = getUnknowns(secondInequation);
 
-    if (
-      !equationsHaveTheSameUnknowns(
-        firstInequalityUnknownsName,
-        secondInequalityUnknownsName
-      ) && firstInequalityUnknownsName?.length === 1
-    ) {
-      return false;
-    }
+  if (
+    !equationsHaveTheSameUnknowns(
+      firstInequalityUnknownsName,
+      secondInequalityUnknownsName
+    ) &&
+    firstInequalityUnknownsName?.length === 1
+  ) {
+    return false;
+  }
 
-    const firstInequalities = breakInequality(firstInequation);
-    const secondInequalities = breakInequality(secondInequation);
+  const firstInequalities = breakInequality(firstInequation);
+  const secondInequalities = breakInequality(secondInequation);
 
-    let firstInequalitiesSolution: xRange = {
-      inferiorLimit: getLimit(firstInequalities, "inferior"),
-      superiorLimit:getLimit(firstInequalities, "superior"),
-    };
+  let firstInequalitiesSolution: xRange = {
+    inferiorLimit: getLimit(firstInequalities, "inferior"),
+    superiorLimit: getLimit(firstInequalities, "superior"),
+  };
 
-    let secondInequalitiesSolution: xRange = {
-      inferiorLimit:getLimit(secondInequalities, "inferior"),
-      superiorLimit: getLimit(secondInequalities, "superior"),
-    };
+  let secondInequalitiesSolution: xRange = {
+    inferiorLimit: getLimit(secondInequalities, "inferior"),
+    superiorLimit: getLimit(secondInequalities, "superior"),
+  };
 
-  return firstInequalitiesSolution.inferiorLimit === secondInequalitiesSolution.inferiorLimit && firstInequalitiesSolution.superiorLimit === secondInequalitiesSolution.superiorLimit
+  return (
+    firstInequalitiesSolution.inferiorLimit ===
+      secondInequalitiesSolution.inferiorLimit &&
+    firstInequalitiesSolution.superiorLimit ===
+      secondInequalitiesSolution.superiorLimit
+  );
 };
